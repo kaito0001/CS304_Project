024b6acf653c82df2a89a667850f8eeeb0b01151
Ahmed samy <105397731+ahmedd-22@users.noreply.github.com>
2023-12-11 01:11:46 +0200

    update

--- src/gameplay/Players/AnimationListener.java
+++ src/gameplay/Players/AnimationListener.java
@@ -1,9 +1,10 @@
 package gameplay.Players;
 
 import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
 import javax.media.opengl.GLEventListener;
 
-public abstract class AnimationListener implements GLEventListener, KeyListener {
+public abstract class AnimationListener implements GLEventListener, KeyListener, MouseListener {
 
     protected String assetsFolderName = "Assets//";
 
